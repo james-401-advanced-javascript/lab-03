a5cda7d66b17abe6636824b20664bd33bea5e71a
James Dunn <jamesdu@zillowgroup.com>
2019-10-08 22:44:16 -0700

    Updated Docs

--- index.js
+++ index.js
@@ -10,3 +10,26 @@ let file = process.argv.slice(2);
 // readCallback(file);
 readPromise(file);
 // readAsync(file);
+
+/**
+ * Simple Server
+ * @module index
+ */
+
+const express = require('express');
+
+const app = express();
+
+app.use('/docs', express.static('./docs'));
+
+app.get('/', requestHandler);
+
+function requestHandler(req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  res.statusCode = 200;
+  res.end();
+}
+
+app.listen(process.env.PORT, () =>
+  console.log(`server up on port ${process.env.PORT}`)
+);
